8953f26e483ebf8b8a61531e89fecf54fa81ed5b
Sidney <sidneywells101@gmail.com>
2020-11-12 21:49:02 -0600

    small updates

--- src/server/index.js
+++ src/server/index.js
@@ -16,31 +16,31 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 app.get('/latest/:rover', async (req, res) => {
   const rover = req.params.rover;
   try {
-    let image = await fetch(
+    let response = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`
     );
-    const data = await image.json();
+    const data = await response.json();
     const latest = data.latest_photos;
     const value = 2;
     res.send({ latest, value });
   } catch (err) {
-    console.log('error:', err);
+    throw new Error('Something went wrong', err);
   }
 });
 
 app.get('/images/:rover', async (req, res) => {
   const rover = req.params.rover;
   try {
-    let image = await fetch(
+    let response = await fetch(
       `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=10&api_key=${process.env.API_KEY}`
     );
-    const data = await image.json();
+    const data = await response.json();
     const roverImages = data.photos.splice(0, 5);
     const roverData = Map(data.photos[0]);
     const value = 1;
     res.send({ roverData, roverImages, value });
   } catch (err) {
-    console.log('error:', err);
+    throw new Error('Something went wrong', err);
   }
 });
 
