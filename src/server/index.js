90fe4bbd1264862ce0bc346baf9f618da3578be2
Sidney <sidneywells101@gmail.com>
2020-11-14 09:22:10 -0600

    fetch error handling

--- src/server/index.js
+++ src/server/index.js
@@ -15,33 +15,47 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.get('/latest/:rover', async (req, res) => {
   const rover = req.params.rover;
-  try {
-    let response = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`
-    );
-    const data = await response.json();
-    const latest = data.latest_photos;
-    const value = 2;
-    res.send({ latest, value });
-  } catch (err) {
-    throw new Error('Something went wrong', err);
-  }
+  fetch(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=${process.env.API_KEY}`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong', err);
+      }
+    })
+    .then((responseJson) => {
+      const latest = responseJson.latest_photos;
+      const value = 2;
+      res.send({ latest, value });
+    })
+    .catch((error) => {
+      throw new Error('This is the error', error);
+    });
 });
 
-app.get('/images/:rover', async (req, res) => {
+app.get('/images/:rover', (req, res) => {
   const rover = req.params.rover;
-  try {
-    let response = await fetch(
-      `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=10&api_key=${process.env.API_KEY}`
-    );
-    const data = await response.json();
-    const roverImages = data.photos.splice(0, 5);
-    const roverData = Map(data.photos[0]);
-    const value = 1;
-    res.send({ roverData, roverImages, value });
-  } catch (err) {
-    throw new Error('Something went wrong', err);
-  }
+  fetch(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=10&api_key=${process.env.API_KEY}`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Something went wrong', err);
+      }
+    })
+    .then((responseJson) => {
+      const roverImages = responseJson.photos.splice(0, 5);
+      const roverData = Map(responseJson.photos[0]);
+      const value = 1;
+      res.send({ roverData, roverImages, value });
+    })
+    .catch((error) => {
+      throw new Error('This is the error', error);
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
